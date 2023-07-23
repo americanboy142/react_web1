851b830c51268afcb3cbddc0d293423748b45053
jack phreaner <jphreaner142@gmail.como>
2023-07-23 21:18:32 +0200

    target for instagram

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -13,7 +13,7 @@ const Contact = () => {
                     <h1 className="main-title" id="home">Contact us</h1>
                     <p>Phone: (805) 452-5688</p>
                     <p>Address: passo walk</p>
-                    <p>Instagram: <Link to='https://www.instagram.com/viacaravaggio147/'>@viacaravaggio147</Link></p>
+                    <p>Instagram: <Link to='https://www.instagram.com/viacaravaggio147/' target="_blank">@viacaravaggio147</Link></p>
                 </div>
             </div>
         </div>
